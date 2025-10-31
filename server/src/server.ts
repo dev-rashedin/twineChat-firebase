f2a0a7b1bb4fe16daf3f2d4a312a72899293eccc
Rashedin-063 <rashedinislam.06@gmail.com>
2025-10-31 08:49:18 +0600

    lin-staged setup done, mongoose installed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,17 @@
+import { connectDB } from "../src/app/config/connectDB";
 import 'dotenv/config';
 import app from './app';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port http://localhost:${port}`);
-});
+(async () => {
+  try {
+    await connectDB();
+    app.listen(config.port, () => {
+      console.log(`Server listening on port http://localhost:${config.port}`);
+    });
+  } catch (err) {
+    console.error('Failed to connect DB or start server:', err);
+    process.exit(1);
+  }
+})();
